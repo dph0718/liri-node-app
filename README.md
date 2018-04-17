add6e403206cf7b66b2aad790e78ebf56279a734
David <dph0718@hotmail.com>
2018-04-17 14:09:14 -0600

    Now with menu!

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 `node liri.js my-twitter`
 ### He can even read a txt file and assume what you want done!
 `node liri.js do-it`
+### Or if you didn't read this README and don't know what to do:
+`node liri.js`
+and Liri will be as helpful as an automated phone response system.
--- liri.js
+++ liri.js
@@ -1,16 +1,26 @@
 require("dotenv").config();
 const fs = require('fs');
+const inquirer = require('inquirer');
 const request = require('request');
 const keys = require('./keys.js');
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
 const client = new Twitter(keys.twitter);
 const spotify = new Spotify(keys.spotify);
+let path;
+let mode;
 
 let command = process.argv[2];
 let value = process.argv[3];
 function stripe() {
     console.log("===================");
+}
+function menuExit() {
+    if (mode == 'menu') {
+        firstMenu();
+    } else {
+        process.exit();
+    }
 }
 
 //flavors this assignment with barbecue sauce.
@@ -18,6 +28,76 @@ function gotRdun() {
     console.log(`\nGot 'R Dun!`);
 }
 
+function firstThing() {
+    inquirer.prompt({
+        name: 'doFirst',
+        message: 'You wanna type commands for me or choose from a list?',
+        type: 'list',
+        choices: ["I need a menu", "YOU LISTEN TO MY WORDS, LIRI."],
+    })
+        .then(function (resp) {
+            if (resp.doFirst == "YOU LISTEN TO MY WORDS, LIRI.") {
+                console.log("Well you think you just know everything, doncha?");
+            } else {
+                firstMenu();
+            }
+        })
+}
+
+
+function firstMenu() {
+    if (mode == 'menu') {
+        path = undefined;
+        value = undefined;
+        inquirer.prompt({
+            name: 'menu1',
+            type: 'list',
+            message: "What you wanna do?",
+            choices: ["find a song", "git a movie", "git my tweets", "git r' dun", "nothin"]
+        })
+            .then(function (resp) {
+                if (resp.menu1 == "nothin") {
+                    console.log("K. Go do that somewhere else.");
+                    setTimeout(function () { process.exit() }, 1000);
+                } else if (resp.menu1 == "git r' dun") {
+                    path = 'txt';
+                    doRandomTxt();
+                } else if (resp.menu1 == "git my tweets") {
+                    twit();
+                } else {
+                    if (resp.menu1 == "find a song") {
+                        path = 'song';
+                    }
+                    if (resp.menu1 == "git a movie") {
+                        path = 'movie';
+                    }
+
+                    secondMenu();
+                }
+            })
+    } else {
+        process.exit();
+    }
+}
+
+function secondMenu() {
+    inquirer.prompt({
+        name: 'menu2',
+        type: 'input',
+        message: "What you lookin' for?",
+    })
+        .then(function (resp) {
+            console.log("Gittin R' dun...")
+            if (path == 'movie') {
+                value = resp.menu2;
+                movie();
+            }
+            if (path == 'song') {
+                value = resp.menu2;
+                spotTune();
+            }
+        })
+}
 
 //gets the last 10 twits from, i assume, whoever's consumer key & secret are supplied.
 function twit() {
@@ -32,6 +112,7 @@ function twit() {
             console.log(`Posted: `, tweets[i].created_at);  // Raw response object. 
         }
         gotRdun();
+        menuExit();
     });
 }
 
@@ -41,7 +122,7 @@ function spotTune() {
     if (value) {
         song = value;
     } else {
-        song = `You Should Consider`;
+        song = `tonight i'm gonna rock you tonight`;
     };
     stripe();
     spotify.search({ type: 'track', limit: 1, query: song })
@@ -54,8 +135,10 @@ function spotTune() {
             console.log("Preview Link:", data.tracks.items[0].album.external_urls.spotify);
             stripe();
             gotRdun();
+            firstMenu();
         }, function (err) {
             console.log("This went terribly wrong: ", err);
+            firstMenu();
         })
 }
 
@@ -79,16 +162,21 @@ function movie() {
         console.log("Actors: " + resp.Actors);
         console.log("Country: " + resp.Country);
         console.log("Language: " + resp.Language)
-        console.log("IMDB Rating: " + resp.Ratings[0].Value);
-        console.log("Rotten Tomatoes Rating: " + resp.Ratings[1].Value);
+        if (resp.Ratings[0]) {
+            console.log("IMDB Rating: " + resp.Ratings[0].Value);
+        } else console.log('');
+        if (resp.Ratings[1]) {
+            console.log("Rotten Tomatoes Rating: " + resp.Ratings[1].Value);
+        } else console.log('');
         console.log("Plot: " + resp.Plot);
         stripe();
         gotRdun();
+        firstMenu();
     })
 }
 
 //reads random.txt and parses it into a readable command and value
-//then puts those values into the ultimatelyDo function.
+//then puts those values into the textInterpret function.
 function doRandomTxt() {
     fs.readFile('random.txt', 'utf8', function (error, data) {
         if (error) throw err;
@@ -97,13 +185,12 @@ function doRandomTxt() {
         let val = content[1];
         command = doIt;
         value = val;
-        ultimatelyDo();
+        textInterpret();
     })
-
 }
 
 //determines what the command is and passes it to the correct function.
-function ultimatelyDo() {
+function textInterpret() {
     if (command === "my-tweets") {
         twit();
     }
@@ -119,5 +206,10 @@ function ultimatelyDo() {
     console.log("Gittin 'R Dun...")
 }
 
-//ultimately does
-ultimatelyDo();
+if (command) {
+    mode = "talk";
+    textInterpret();
+} else {
+    mode = "menu";
+    firstThing();
+}
